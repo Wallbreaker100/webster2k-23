03c94ecc1dff292dc8c9905e91817b2af1068dd3
sarthak patel <patelsarthak421@gmail.com>
2023-11-09 03:08:59 +0530

    added login signup

--- src/components/homecomponents/Description.js
+++ src/components/homecomponents/Description.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './../../css/navbar.css'
-
+import { useAuth0 } from "@auth0/auth0-react";
 const Description = () => {
+  const { user,loginWithRedirect } = useAuth0();
+  console.log(user);
   return (
     <div className='outerdesdiv'>
       <div className='innerdesdiv'>
         <div className='innerdesdiv1'>
           <h1>Draw Your <span>Imagination,</span><br/>Guess the <span>Creation!</span></h1>
           <p>"Picasso is a fun and creative drawing and guessing game that challenges players to communicate without words."</p>
-          <button>LOG IN</button>
+          <button onClick={() => loginWithRedirect({screen_hint:"/room"})}>LOG IN</button>
         </div>
         <div className='innerdesdiv2'>
           <img className="desimg" src="https://app.svgator.com/assets/svgator.webapp/log-in-girl.svg"/>
